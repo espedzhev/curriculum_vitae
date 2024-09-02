042fd678d95a7c30c284ac320cb5bd00e218a5f4
espedzhev <evald.spedzhev@gmail.com>
2024-09-02 16:31:00 +0100

    rework

--- to_html.py
+++ to_html.py
@@ -1,7 +1,7 @@
 from markdown2 import markdown
-from weasyprint import HTML
 
 with open('README.md', 'r', encoding='utf-8') as file:
     markdown_content = file.read()
 
-HTML(string=markdown(markdown_content)).write_pdf('evald_spedzhev_cv_2024.pdf')
+with open('cv_output.html', 'w', encoding='utf-8') as html_file:
+    html_file.write(markdown(markdown_content))
