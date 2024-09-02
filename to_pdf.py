26a086673f2a055b50125addac06856ae80f0119
espedzhev <evald.spedzhev@gmail.com>
2024-09-02 16:26:29 +0100

    rework

--- to_pdf.py
+++ to_pdf.py
@@ -8,6 +8,11 @@ with open('README.md', 'r', encoding='utf-8') as file:
 html_content = markdown(markdown_content)
 pdf_file_path = 'evald_spedzhev_cv_2024.pdf'
 
+# Save the HTML content to an HTML file
+html_file_path = 'cv_output.html'
+with open(html_file_path, 'w', encoding='utf-8') as html_file:
+    html_file.write(html_content)
+
 # Generate PDF from HTML
 pdf_file_path = 'evald_spedzhev_cv_2024.pdf'
 HTML(string=html_content).write_pdf(pdf_file_path)
